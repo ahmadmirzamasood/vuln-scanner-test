34091f9ba1f7bf2d6011d387f9559744c911a949
Masood Ahmad <142745366+ahmadmirzamasood@users.noreply.github.com>
2026-06-01 00:36:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import jwt
 def get_user(user_id):
     conn = sqlite3.connect("db.sqlite")
     query = f"SELECT * FROM users WHERE id = {user_id}"
-    return conn.execute(query).fetchone()
+    return conn.execute(query).fetchone()"
 
 SECRET_KEY = "hardcoded_secret_key_123"
 
